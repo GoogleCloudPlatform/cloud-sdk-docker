a6994b4a25920be8195025e305c8001099f8d445
Johan Euphrosine <proppy@google.com>
2014-11-26 12:55:52 -0800

    test: add usage

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ set -ex
 HASH=$(git rev-parse HEAD)
 GCLOUD_SDK=cloud-sdk:$HASH
 GCLOUD_CONFIG=gcloud-config-$HASH
+PROJECT=${PROJECT:?missing, usage: PROJECT=GCLOUD-PROJECT test.sh}
 
 docker build -t ${GCLOUD_SDK} .
 docker rm ${GCLOUD_CONFIG} || true
