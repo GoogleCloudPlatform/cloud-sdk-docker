19f9addeba806845cd419e8a3909ae83216cfd5f
Will Faris <wfaris@google.com>
2020-10-27 20:04:36 +0000

    rm retag.sh

--- retag.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export VERSION=291.0.0 && \
-  git add --all && git commit -m "Update SDK to $VERSION" --allow-empty && \
-  git tag --force  -a $VERSION -m "v${VERSION}" && \
-  git push --force origin master --tags "${VERSION}"
