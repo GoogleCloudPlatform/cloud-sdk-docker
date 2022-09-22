b6e757ecea51c2e2198a6747a333eda29da85dcc
Nathan Looney <nathanlooney@google.com>
2022-09-22 20:51:53 +0000

    Remove emulators from multiarch support until we determine why it won't build on bullseye

--- generate_cloudbuild.py
+++ generate_cloudbuild.py
@@ -51,7 +51,7 @@ DOCKERHUB_PREFIX='google'
 OLD_NAME='cloud-sdk'
 REBRAND_NAME='google-cloud-cli'
 IMAGES=['alpine', 'debian_slim', 'default', 'debian_component_based', 'emulators']
-MULTI_ARCH=['debian_slim', 'debian_component_based', 'alpine', 'emulators']
+MULTI_ARCH=['debian_slim', 'debian_component_based', 'alpine']
 LABEL_FOR_IMAGE={
     'alpine': 'alpine',
     'debian_slim': 'slim',
