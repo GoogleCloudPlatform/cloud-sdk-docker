5c4f797ddd7154b3d815c9eaa6c6b5949b165797
Md Anindya Prodhan <mdanindya@google.com>
2024-12-27 20:16:43 +0000

    Add smoke tests for components installed in the emulators and latest docker images

---
+++ emulators/emulator-testing.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y lsof
+
+# Start the server in the background
+gcloud beta emulators $1 start --host-port=0.0.0.0:8085 --project=blah &
+
+# Wait a few seconds for the server to start
+sleep 10
+
+# Check if the server is listening on the port
+if lsof -i :8085 > /dev/null; then
+  echo "Server started successfully (exiting with code 0)"
+  kill -9 $(lsof -t -i :8085)
+  exit 0
+else
+  echo "Server failed to start"
+  exit 1
+fi
