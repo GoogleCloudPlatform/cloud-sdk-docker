d5b7ce99e93de4e0a3b272059d25305d5f10e8eb
Md Anindya Prodhan <mdanindya@google.com>
2025-01-25 23:51:03 +0000

    Add testing scripts to the docker images

---
+++ emulators/test-all-emulators.sh
@@ -0,0 +1,43 @@
+
+#!/bin/bash
+
+./emulator-testing.sh pubsub
+if [ $? -ne 0 ]; then 
+
+    echo "Pub/sub emulator failed to execute."
+    exit 1  
+
+fi
+
+./emulator-testing.sh datastore
+if [ $? -ne 0 ]; then 
+
+    echo "Datastore emulator failed to execute."
+    exit 1  
+
+fi
+
+./emulator-testing.sh firestore
+if [ $? -ne 0 ]; then
+
+    echo "Firestore emulator failed to execute."
+    exit 1
+
+fi
+
+./emulator-testing.sh spanner
+if [ $? -ne 0 ]; then 
+
+    echo "Spanner emulator failed to execute."
+    exit 1  
+
+fi
+
+./emulator-testing.sh bigtable
+if [ $? -ne 0 ]; then
+
+    echo "Bigtable emulator failed to execute."
+    exit 1
+
+fi
+
